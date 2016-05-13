abc0fae78323692d371c758f9d7db006f20fce47
Johan Reimegård <johan.reimegard@gmail.com>
2016-05-13 14:45:45 +0200

    made the script less hardcoded and added so that it can take only one as input.

--- R/plot_rna_lib_summary.R
+++ R/plot_rna_lib_summary.R
@@ -10,9 +10,16 @@ plot.header <- args[2]
 out.plot.file <- args[3]
 out.tab.file <- args[4]
 
+input.dir = "/Users/johanreimegard/Vetenskap/Data/jakobsson/nobackup/RIP/tmp"
+plot.header <- "test"
+out.plot.file <- "/Users/johanreimegard/Vetenskap/Data/jakobsson/nobackup/RIP/tmp/tmp.pdf"
+out.tab.file <-"/Users/johanreimegard/Vetenskap/Data/jakobsson/nobackup/RIP/tmp/tmp.txt"
+
 ## Hard coded values
 suffix <- "_lengths.txt"
-biotypes <- c("miRNA", "tRNA", "snRNA", "snoRNA", "rRNA", "protein_coding", "lincRNA", "pseudogene", "repeat")
+files =  list.files(path = input.dir, full.names = FALSE)
+biotypesFiles = files[grep(pattern = suffix, x = files)]
+biotypes  = gsub(pattern = suffix,replacement = "", x = biotypesFiles)
 lwd=3
 xlim=c(15,48)
 show.lengths <- as.character(xlim[1]:xlim[2])
@@ -21,6 +28,7 @@ show.lengths <- as.character(xlim[1]:xlim[2])
 ##################
 ## Read data, allow missing values (which will be set to 0)
 input.files <- list.files(path=input.dir, pattern=paste("*", suffix, sep=""), full.names=TRUE)
+
 
 length.count.mat <- matrix(0, ncol=length(biotypes), nrow=length(show.lengths))
 rownames(length.count.mat) <- show.lengths
@@ -36,9 +44,10 @@ for (input.file in input.files){
 	length.count.mat[names(new.lengths),new.biotype] <- new.lengths
 }
 
-
 ## Sort matrix
-length.count.mat <- length.count.mat[, biotypes]
+if(ncol(length.count.mat) > 1){
+  length.count.mat <- length.count.mat[, biotypes]
+}
 
 
 ## Print table with combined data
@@ -51,7 +60,11 @@ pdf(out.plot.file, width=7, height=7)
 par(mfrow=c(2,2), oma = c(0, 0, 2, 0))
 
 ## 1) Plot overall length distribution
-length.distr <- apply(length.count.mat,1,sum)
+if(ncol(length.count.mat) > 1){
+  length.distr <- apply(length.count.mat,1,sum)
+}else{
+  length.distr <- length.count.mat[,1]
+}
 length.distr <- length.distr[show.lengths]
 plot(x=names(length.distr), y=length.distr, type="l", lwd=lwd, col="gray",
 		 xlab="nucleotides", ylab="nr reads")
@@ -68,14 +81,20 @@ barplot(biotypes, las=3, col = cols, ylab="nr reads")
 ## 3) Length distribution within each biotype
 cols <- rainbow(ncol(length.count.mat))
 legend.cex=0.6
+if(ncol(length.count.mat) > 1){
+  plot.data <- length.count.mat[as.character(xlim[1]:xlim[2]),]
+}else{
+  plot.data <- length.count.mat
+}
 
-plot.data <- length.count.mat[as.character(xlim[1]:xlim[2]),]
 
 
 plot(x=rownames(plot.data), y=plot.data[,1], type="l", col=cols[1],
      lwd=lwd, xlim=xlim, ylim=c(0, max(plot.data)), xlab="nucleotides", ylab="nr reads")
-for (i in 2:ncol(plot.data)){
+if(ncol(length.count.mat) > 1){
+  for (i in 2:ncol(plot.data)){
     lines(x=rownames(plot.data), y=plot.data[,i], col=cols[i], lwd=lwd)
+  }
 }
 legend(x="topright", fill=cols[1:ncol(plot.data)], legend=colnames(plot.data), cex=legend.cex)
 
@@ -85,8 +104,10 @@ plot.data <- t(t(plot.data)/apply(plot.data, 2, sum)) ## Normalize for each biot
 
 plot(x=rownames(plot.data), y=plot.data[,1], type="l", col=cols[1],
      lwd=lwd, xlim=xlim, ylim=c(0, max(plot.data)), xlab="nucleotides", ylab="fraction reads")
-for (i in 2:ncol(plot.data)){
+if(ncol(length.count.mat) > 1){
+  for (i in 2:ncol(plot.data)){
     lines(x=rownames(plot.data), y=plot.data[,i], col=cols[i], lwd=lwd)
+  }
 }
 legend(x="topright", fill=cols[1:ncol(plot.data)], legend=colnames(plot.data), cex=legend.cex)
 
